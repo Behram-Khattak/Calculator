757ae4ab3a920fead67896a18079584000e049dc
Behram Khattak <81041662+Behram-Khattak@users.noreply.github.com>
2022-10-05 11:39:40 +0500

    Building a command line calculator

---
+++ calculator.py
@@ -0,0 +1,12 @@
+print("Enter the 1st number you want to calculate")
+
+getFirstInteger = input()
+
+print("Enter the calculation symbol !")
+
+getSymbol = input()
+
+print("Enter the 2nd number you want to calculate")
+
+getSecondInteger = input()
+
